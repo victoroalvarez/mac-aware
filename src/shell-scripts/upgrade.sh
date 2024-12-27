956cd87f6b370355b02de96eac77f60b061df60a
Victor O. Alvarez <voa@keemail.me>
2024-12-27 10:34:06 -0400

    Update Upgrade Script.

--- src/shell-scripts/upgrade.sh
+++ src/shell-scripts/upgrade.sh
@@ -2,13 +2,15 @@
 # Upgrade PKGSRC & Brew for macOS
 
 # Upgrade PKGSRC
-echo 'Upgrade PKGSRC';
+echo 'Upgrade PKGSRC:';
 sudo pkgin update && sudo pkgin upgrade;
 
 # Upgrade Brew
-echo 'Upgrade Brew';
+echo ''
+echo 'Upgrade Brew:';
 brew update && brew upgrade;
 
 # Upgrade Mac App Store through MAS CLI tool
-echo "Upgrade Mac App Store Apps through MAS CLI tool";
+echo ''
+echo "Upgrade Mac App Store Apps through MAS CLI tool:";
 mas upgrade;
