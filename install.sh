46b3101bd339c3148731c2ae1f5b4c5d39ef2e57
Victor O. Alvarez <victoroalvarez@protonmail.com>
2026-05-15 23:34:38 -0400

    Improve install.sh install script.

--- install.sh
+++ install.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
-# install-dotfiles
-# Dotfiles install script for macOS
+# install.sh
+# Dotfiles Install script for macOS
 
-# Create a bin, builds & scripts directory on $HOME directory
+# Create a .config, Downloads/Youtube_Downloads, bin, builds directories on $HOME
 mkdir -p $HOME/.config
 mkdir -p $HOME/Downloads/Youtube_Downloads
 mkdir -p $HOME/bin
 mkdir -p $HOME/builds
 mkdir -p $HOME/development
 mkdir -p $HOME/projects
-mkdir -p $HOME/projects-python
 
 # Create ZSH files
 touch $HOME/.zsh_history
